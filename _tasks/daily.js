c87a50f58f2ff2dcbca8438ab6fd067455dd8b97
Jim Newbery <james@tinnedfruit.com>
2018-06-25 09:23:06 +0100

    Allow daily task to create email with day offset argument

--- _tasks/daily.js
+++ _tasks/daily.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const { exec } = require('child_process');
 
-const date = new Date().toISOString().split('T')[0];
+const dayOffset = Number(process.argv[2] || 0);
+
+let d = new Date();
+d.setDate(d.getDate() + dayOffset);
+
+const date = d.toISOString().split('T')[0];
 
 const contents = `---
 title: 
@@ -16,11 +22,17 @@ All the best,
 const filename = `${date.replace(/\-/g, '')}.md`;
 const filepath = path.resolve(__dirname, '..', '_list', filename);
 
-fs.writeFile(filepath, contents, err => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Created file ${filename}`);
-    process.exit(0);
+exec(`git checkout -b email-${date.replace(/\-/g, '')}`, function(e) {
+  if (e) {
+    console.error(e);
   }
+
+  fs.writeFile(filepath, contents, err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Created file ${filename}`);
+      process.exit(0);
+    }
+  });
 });
